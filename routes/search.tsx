61454c2eb9c2633e9716440d987e8abe681eb6a5
takusan64 <hafe1119@yahoo.co.jp>
2022-10-10 01:28:55 +0900

    first commit

---
+++ routes/search.tsx
@@ -0,0 +1,32 @@
+import { Handlers, PageProps } from "$fresh/server.ts";
+
+const NAMES = ["Alice", "Bob", "Charlie", "Dave", "Eve", "Frank"];
+
+interface Data {
+  results: string[];
+  query: string;
+}
+
+export const handler: Handlers<Data> = {
+  GET(req, ctx) {
+    const url = new URL(req.url);
+    const query = url.searchParams.get("q") || "";
+    const results = NAMES.filter((name) => name.includes(query));
+    return ctx.render({ results, query });
+  },
+};
+
+export default function Page({ data }: PageProps<Data>) {
+  const { results, query } = data;
+  return (
+    <div class="p-4">
+      <form>
+        <input class="border-2 border-black p-1" type="text" name="q" value={query} />
+        <button class="border-2 border-blue-900 p-1" type="submit">Search</button>
+      </form>
+      <ul>
+        {results.map((name) => <li key={name}>{name}</li>)}
+      </ul>
+    </div>
+  );
+}
